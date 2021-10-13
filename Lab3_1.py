ec107953bf647dca062302c76f49b8fec5b41ac3
malevrovich <malevrovich@LAPTOP-4QT8RMP5.localdomain>
2021-10-13 21:55:36 +0300

    some fixes

--- Lab3_1.py
+++ Lab3_1.py
@@ -1,49 +1,14 @@
 import re
+import sys
 
-inp = input()
+def main(argv):
+	inp = argv[1]
 
-id = 335146
+	print("INP: ", inp)
+	print("RES: ", len(re.findall(r';-{\(', inp)))
 
-smile = ""
-
-if id % 5 == 0:
-	smile += ":"
-elif id % 5 == 1:
-	smile += ";"
-elif id % 5 == 2:
-	smile += "X"
-elif id % 5 == 3:
-	smile += "8"
-else:
-	smile += "="
-
-if id % 4 == 0:
-	smile += "-"
-elif id % 4 == 1:
-	smile += "<"
-elif id % 4 == 2:
-	smile += "-{"
-else:
-	smile += "<{"
-
-if id % 7 == 0:
-	smile += "("
-elif id % 7 == 1:
-	smile += ")"
-elif id % 7 == 2:
-	smile += "O"
-elif id % 7 == 3:
-	smile += "|"
-elif id % 7 == 4:
-	smile += "\\"
-elif id % 7 == 5:
-	smile += "/"
-else:
-	smile += "P"
-
-print(smile)
-
-print(len(re.findall(r';-{\(', inp)))
+if __name__ == "__main__":
+	main(sys.argv)
 
 #TESTS
 #;-{( abs;-{(  ;-}( :+;{ ;--{(;;-{(;-{(  ::;-{()( => 5
--- Lab3_3.py
+++ Lab3_3.py
@@ -1,7 +1,14 @@
 import re
+import sys
 
 def repl(m):
 	num = int(m[0])
 	return str(3 * num ** 2 + 5)
 
-print(re.sub(r'-?\d+', repl, input()))
+def main(argv):
+	inp = argv[1]
+	print("INP: ", inp)
+	print("RES: ", re.sub(r'-?\d+', repl, inp))
+
+if __name__ == "__main__":
+	main(sys.argv)
